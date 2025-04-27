483544ae60cdb5509eec1211a4530a4f20eb8a79
Sunny152365 <sonnentag0609@gmail.com>
2025-04-27 23:02:48 +0900

    base Calender w. GPT

---
+++ ToyProject_Swift_Calender/ToyProject_Swift_Calender/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ToyProject_Swift_Calender
+//
+//  Created by 최원일 on 4/27/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ToyProject_Swift_Calender/ToyProject_Swift_CalenderTests/ToyProject_Swift_CalenderTests.swift
@@ -0,0 +1,17 @@
+//
+//  ToyProject_Swift_CalenderTests.swift
+//  ToyProject_Swift_CalenderTests
+//
+//  Created by 최원일 on 4/27/25.
+//
+
+import Testing
+@testable import ToyProject_Swift_Calender
+
+struct ToyProject_Swift_CalenderTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
