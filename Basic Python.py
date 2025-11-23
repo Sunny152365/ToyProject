4814562d380b5eb576236f458a197cdff7a33779
Sunny152365 <sonnentag0609@gmail.com>
2025-11-23 22:07:58 +0900

    Update Basic Python.py

--- Basic Python.py
+++ Basic Python.py
@@ -1,10 +1,15 @@
 #01 인터프리터 언어 : 소스 코드를 한 줄씩 읽어서 바로 실행하는 언어
 print("Hello World!")
 
-#02 텍스트 에디터 : 각 코드 . 줄옆에 써있는 설명(즉, n번째 줄)
+#02 텍스트 에디터 : 라인 번호
 print('안녕하세요')
 a = 1
 b = 1
 print(a + b)
 
-#03 
+#03 변수명 만들기
+_myname = 'samsjang'
+my_name = '홍길동'
+MyName2 = 'Hong gil-dong'
+counter = 1
+Counter = 2
