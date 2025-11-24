241153c9fb5aa97ab6f52c2c8e459b3108217f22
Sunny152365 <sonnentag0609@gmail.com>
2025-11-24 13:48:36 +0900

    Update Basic Python.py

--- Basic Python.py
+++ Basic Python.py
@@ -7,9 +7,24 @@ a = 1
 b = 1
 print(a + b)
 
-#03 변수명 만들기
+#03 변수명 만들기 : 변수명의 첫 문자는 _또는 영문자, 대소문자 구분, 숫자 사용 가능(단, 첫 문자 제외)
 _myname = 'samsjang'
 my_name = '홍길동'
 MyName2 = 'Hong gil-dong'
 counter = 1
 Counter = 2
+
+#04 변수에 값 대입하기 : 따로 자료형태 선언 없이 값 대입 시 자동으로 자료형 결정
+number1 = 1
+pi = 3.14
+flag = True
+char = 'x'
+chars = 'I love Python'
+
+#05 주석 # 
+# 만든 날짜 : 2025.11.23
+a = 1
+b = 5
+print(a+b)
+
+#06  
