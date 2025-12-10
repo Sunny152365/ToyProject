ada19915f3911190116c877e629634781b2ea1ca
Sunny152365 <sonnentag0609@gmail.com>
2025-12-10 22:46:31 +0900

    Update Basic_Python_200.py

--- Basic_Python_200.py
+++ Basic_Python_200.py
@@ -985,10 +985,18 @@ pos = soloarsys.index(planet)
 soloarsys[pos] = 'Mars'
 print(solarsys)
 
-#103 
+#103 리스트에서 특정 구간에 있는 요소 추출
+solarsys = ['태양', '수성', '금성', '지구', '화성', '목성', '토성', '천왕성', '해왕성']
+rock_planets = solarsys[1:4]
+gas_planets = solarsys[4:]
+print('태양계의 암석형 행성:',end='');print(rock_planets)
+print('태양계의 가스형 행성:',end='');print(gas_planets)
 
+#104 리스트에서 짝수 번째 요소만 추출
+listdata = list(range(1, 21))
+evenlist = listdata[1::2]
+print(evenlist)
 
-#104
 #105
 #106
 #107
