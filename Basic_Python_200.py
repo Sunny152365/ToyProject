3340bbae1a0664dedb5bb752f1f91887ec97847a
Sunny152365 <sonnentag0609@gmail.com>
2025-12-19 11:30:53 +0900

    Update Basic_Python_200.py

--- Basic_Python_200.py
+++ Basic_Python_200.py
@@ -1480,10 +1480,40 @@ else:
     print('[%s](는) 이미 존재합니다.' %dir_name)
 
 #156 파일인지 디렉터리인지 확인(os.path.isfile, os.path.isdir)
+import os
+from os.path import exists, isdir, isfile
+
+files = os.listdir()
+for file in files:
+    if isdir(file):
+        print('DIR: %s' %file)
+
+for file in files:
+    if isfile(file):
+        print('FILE: %s' %file)
+
 #157 현재 시간을 년-월-일 시:분:초로 출력(localtime, strftime)
+from time import localtime, strftime
+
+logfile = 'test.log'
+def writelog(logfile, log):
+    time_stamp = strftime('%Y-%m-%d %X\t', localtime())
+    log = time_stamp + log + '\n'
+    
+    with open(logfile, 'a') as f:
+        f.writelines(log)
+        
+writelog(logfile, '첫 번째 로깅 문장입니다.')
+
 #158 올해 경과된 날짜수 계산(localtime)
+
+
 #159 오늘의 요일 계산(localtime)
+
+
 #160 프로그램 실행 시간 계산(datetime.now)
+
+
 #161
 #162
 #163
