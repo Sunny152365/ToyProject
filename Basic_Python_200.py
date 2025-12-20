29c75eb6e1eda3f2507086571e342c223f281c01
Sunny152365 <sonnentag0609@gmail.com>
2025-12-20 09:12:55 +0900

    Update Basic_Python_200.py

--- Basic_Python_200.py
+++ Basic_Python_200.py
@@ -1506,13 +1506,27 @@ def writelog(logfile, log):
 writelog(logfile, '첫 번째 로깅 문장입니다.')
 
 #158 올해 경과된 날짜수 계산(localtime)
+from time import localtime
 
+t = localtime()
+start_day = '%d-01-01' %t.tm_year
+elapsed_day = t.tm_yday
+
+print('오늘은 [%s]이후 [%d]일째 되는 날입니다.' %(start_day, elapsed_day))
 
 #159 오늘의 요일 계산(localtime)
+from time import localtime
 
+weekdays = ['월요일', '화요일', '수요일', '목요일', '금요일', '토요일', '일요일']
+
+t = localtime()
+today = '%d-%d-%d' %(t.tm_year, t.tm_mon, t.tm_mday)
+week = weekdays[t.tm_wday]
+
+print('[%s] 오늘은 [%s]입니다.' %(today, week))
 
 #160 프로그램 실행 시간 계산(datetime.now)
-
+음.
 
 #161
 #162
