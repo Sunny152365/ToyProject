0f0e288607f04417826a3594d64ef0292cc60343
Sunny152365 <sonnentag0609@gmail.com>
2025-12-21 22:32:38 +0900

    Update Basic_Python_200.py

--- Basic_Python_200.py
+++ Basic_Python_200.py
@@ -1526,7 +1526,19 @@ week = weekdays[t.tm_wday]
 print('[%s] 오늘은 [%s]입니다.' %(today, week))
 
 #160 프로그램 실행 시간 계산(datetime.now)
-음.
+from datetime import datetime
+
+start = datetime.now()
+print('1에서 백만까지 더합니다.')
+ret = 0
+for i in range(1000001):
+    ret += i
+print('1에서 백만까지 더한 결과: %d' %ret)
+end = datetime.now()
+elapsed = end - start
+print('총 계산 시간: ', end='');print(elapsed)
+elapsed_ms = int(elapsed.total_seconds()*1000)
+print('총 계산 시간: %dms' %elapsed_ms)
 
 #161
 #162
