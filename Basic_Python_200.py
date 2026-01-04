8632400a22a1480c5f45d30685002609ea5c4dfc
Sunny152365 <sonnentag0609@gmail.com>
2026-01-04 20:08:26 +0900

    Update Basic_Python_200.py

--- Basic_Python_200.py
+++ Basic_Python_200.py
@@ -2503,9 +2503,33 @@ def runServer():
 runServer()
 
 #194 에코 클라이언트 만들기_1
+import socket
+
+HOST = 'localhost'
+PORT = 9009
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+    sock.connect((HOST, PORT))
+    msg = input('메시지 입력: ')
+    sock.sendall(msg.encode())
+    data = sock.recv(1024)
+
+print('에코 서버로부터 받은 데이터 [%s]' %data.decode())
+
 #195 에코 서버 만들기_1
+
+
 #196 에코 클라이언트 만들기_2
+
+
 #197 파일 송신 프로그램 만들기
+
+
 #198 파일 수신 프로그램 만들기
+
+
 #199 채팅 서버 만들기
+
+
 #200 채팅 클라이언트 만들기
+
